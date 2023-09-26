3e19e8f73efdea66fdf3d69e4aa9a8cd235d1c46
Nikunj-Java <ganitniduniyanikunj10@gmail.com>
2023-09-26 16:52:15 +0530

    Angular and typescript notes

---
+++ first.js
@@ -0,0 +1,10 @@
+var a;
+a = 58;
+var b = 12;
+var firstName = "Nikunj Soni";
+function demo(num1, num2) {
+    return num1 * num2;
+}
+console.log(a + b);
+console.log(firstName);
+console.log(demo(10, 20));
---
+++ first.ts
@@ -0,0 +1,15 @@
+
+let a:number;
+
+a=58;
+
+let b:number=12;
+let firstName:string="Nikunj Soni";
+function demo(num1:number,num2:number){
+    return num1*num2;
+}
+console.log(a+b);
+console.log(firstName);
+console.log(demo(10,20));
+
+
